67ba48d86442e138309f94d3d7e2d548405cd9f7
dPerez015 <davidperezgallego@enti.cat>
2020-06-04 16:44:07 +0200

    Added solved exercises from class 4

---
+++ Peces/Assets/Clase3/Script/PhongPointLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhongPointLight : MonoBehaviour
+{
+
+    public Material phongMaterial;
+    int positionID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionID = Shader.PropertyToID("Position");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phongMaterial.SetVector(positionID, transform.position);
+    }
+}
---
+++ Peces/Assets/Clase3/Script/PhongPointLightDirecto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhongPointLightDirecto : MonoBehaviour
+{
+    public Material phongIlumMat;
+    int propId;
+
+    Renderer rend;
+    Material thisMat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propId = Shader.PropertyToID("Position");
+        rend = GetComponent<Renderer>();
+        thisMat = rend.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phongIlumMat.SetVector(propId, transform.position);
+    }
+
+
+}
